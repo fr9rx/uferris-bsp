aacb2c7c2d1ccc3440fe18da0e790bbc98d75ffb
f23 <fahadhiyari0@gmail.com>
2026-06-13 18:54:40 +0300

    Updated The REPO to sync the fork

--- examples/xiao-nrf52840/src/bin/blinky.rs
+++ examples/xiao-nrf52840/src/bin/blinky.rs
@@ -3,7 +3,7 @@
 
 use embassy_executor::Spawner;
 use embassy_nrf::init;
-use embassy_time::Timer;
+use embassy_time::{Duration, Timer};
 use panic_halt as _;
 use uferris_bsp::uferris_init;
 
@@ -13,14 +13,7 @@ async fn main(_s: Spawner) {
     let mut uferris = uferris_init(p);
     let delay: u64 = 300;
     loop {
-        uferris.led3_off().unwrap();
         uferris.led1_on();
-        Timer::after_millis(delay).await;
-        uferris.led1_off();
-        uferris.led2_on().unwrap();
-        Timer::after_millis(delay).await;
-        uferris.led2_off().unwrap();
-        uferris.led3_on().unwrap();
         Timer::after_millis(delay).await;
     }
 }
